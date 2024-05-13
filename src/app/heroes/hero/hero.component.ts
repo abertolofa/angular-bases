60f6e6ef272ab8d22b2328fbec5074fbaff117fb
Ariadna Bertolo Fafian <abertolo@aldaba.es>
2024-05-13 13:40:19 +0200

    fin de seccion 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name : string = "ironman";
+  public age : number = 45;
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase()
+  }
+
+  getHeroDescription():string {
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero() : void {
+    this.name = "Spiderman"
+  }
+  changeAge(): void {
+    this.age = 16
+  }
+  reset(): void{
+    this.age = 45;
+    this.name = "ironman"
+  }
+
+}
